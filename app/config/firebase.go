e929b2b8c98f3a1d0394a2a97cf8923a7283f1f9
bimogempar <bimogempar@gmail.com>
2023-04-10 05:01:06 +0700

    core: done first setup fiebase config and create middleware for access protected route

--- app/config/firebase.go
+++ app/config/firebase.go
@@ -1,1 +1,18 @@
 package config
+
+import (
+	"context"
+	"log"
+
+	firebase "firebase.google.com/go/v4"
+	"google.golang.org/api/option"
+)
+
+func SetupFirebase() *firebase.App {
+	opt := option.WithCredentialsFile("./firebaseServiceKey.json")
+	fb, err := firebase.NewApp(context.Background(), nil, opt)
+	if err != nil {
+		log.Fatalln("Error initializing Firebase App:", err)
+	}
+	return fb
+}
--- app/middlewares/middleware.go
+++ app/middlewares/middleware.go
@@ -1,1 +1,30 @@
 package middlewares
+
+import (
+	"net/http"
+
+	firebase "firebase.google.com/go/v4"
+	"github.com/gofiber/fiber/v2"
+)
+
+func FirebaseAuth(app *firebase.App) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		// Get Firebase ID Token from Authorization header
+		idToken := c.Get("Authorization")[7:] // Remove "Bearer " prefix
+
+		// Verify ID Token
+		fb, err := app.Auth(c.Context())
+		token, err := fb.VerifyIDToken(c.Context(), idToken)
+		if err != nil {
+			return c.Status(http.StatusUnauthorized).JSON(fiber.Map{
+				"error": "Unauthorized",
+			})
+		}
+
+		// Add User to Context
+		c.Locals("user", token)
+
+		// Call next middleware
+		return c.Next()
+	}
+}
